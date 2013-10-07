0384fad9ca1b25dc826ddcaa5150ef96e03db6bd
SOA2013-102078512 <elaine790916@hotmail.com>
2013-10-06 18:01:27 -0700

    1st

---
+++ example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example::Application.config.secret_token = 'cc667ceb66d2eaf0a4b5b70a9a571169abcdb518e48106719a60b59c80ad8f568d2ee2fb6806cae0b185a65a571a883f1095b400444053f284603cd0b7cc2ee2'
