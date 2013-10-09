969cebbf49955831025b520d7642288618daac20
SOA2013-102078512 <elaine790916@hotmail.com>
2013-10-08 23:21:40 -0700

    3rd

---
+++ classSOA/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '00c8526d266b1a256e783d2585d844191e1f12e96d8eaa7926df350ccd62a8d0bc065006a5faf50edd3c9f088495de8fd2d0542dc4b4eb894b9265d4c28021d8'
